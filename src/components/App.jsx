297e0eaec85ccd3eea99cab49aba91fa954dd0c7
Meer Estiyak <mmestiyak@gmail.com>
2021-08-03 03:53:25 -0700

    done

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ChildComponent1 from './ChildComponent1';
+
+const App = () => {
+  return (
+    <>
+      <div>HEY THERE</div>
+      <ChildComponent1 />
+    </>
+  );
+};
+
+export default App;
---
+++ src/redux/action-creators/accountActions.js
@@ -0,0 +1,15 @@
+export const deposit = (amount) => {
+  return (dispatch) =>
+    dispatch({
+      type: 'deposit',
+      payload: amount,
+    });
+};
+
+export const withdraw = (amount) => {
+  return (dispatch) =>
+    dispatch({
+      type: 'withdraw',
+      payload: amount,
+    });
+};
